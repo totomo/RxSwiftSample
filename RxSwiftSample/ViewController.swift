785e4a840e28919b7c4f735b0702f300ca1e568c
Kenta Tokumoto <totomo_1217@yahoo.co.jp>
2016-03-12 22:20:45 +0900

    Add project with RxSwift(by using carthage) initially

---
+++ RxSwiftSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxSwiftSample
+//
+//  Created by Kenta Tokumoto on 2016/03/12.
+//  Copyright © 2016年 Kenta Tokumoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
